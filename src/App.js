7ed883a5b4792a00083860c025b42e9f2a404989
Saransh Bhatia <saranshbht@gmail.com>
2020-08-27 08:54:56 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Badge from "react-bootstrap/Badge"
+let marked = require('marked');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markdown: "",
+    };
+  }
+
+  updateMarkdown(markdown) {
+    this.setState({markdown});
+  }
+
+  render() {
+    var inputStyle = {
+      width: '400px',
+      height: '50vh',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      padding: '0px',
+    };
+
+    var outputStyle = {
+      width: '400px',
+      height: '50vh',
+      backgroundColor: '#DCDCDC',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      padding: '10px',
+      overflow: 'auto' 
+    };
+
+    var textAreaStyle = {
+      width: '100%',
+      height: '100%',
+      margin: '0px'
+    }
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col text-center">
+              <h1>
+                <Badge className="text-align-center" variant="light">
+                  Markdown Previewer
+                </Badge>
+              </h1>
+            </div>
+          </div>
+
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Markdown Input
+                  </Badge>
+                </h4>
+              </div>
+
+              <div className="mark-input" style={inputStyle}>
+                <textarea className="input" style={textAreaStyle} value={this.state.markdown}
+                onChange={
+                  (e) => {
+                    this.updateMarkdown(e.target.value);
+                  }
+                }>
+                  {/* {console.log(this.state.markdown)} */}
+                </textarea>
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="col text-center">
+                <h4>
+                  <Badge className="text-align-center" variant="secondary">
+                    Preview 
+                  </Badge>
+                </h4>
+              </div>
+
+              <div style={outputStyle}
+                dangerouslySetInnerHTML={{__html: marked(this.state.markdown)}}>
+              </div>
+            </div>
+          </div>
+
+          <div className="row mt-4">
+            <div className="col text-center">
+              <button type="button" className="btn btn-primary" 
+              onClick={
+                (e) => {
+                this.updateMarkdown("");
+              }}>Reset</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
